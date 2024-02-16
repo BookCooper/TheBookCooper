13c26471e2d7b72ce33b5ab9e281e7fcfe9190d7
Yichi-liu <alexaa543216@gmail.com>
2024-02-15 20:16:54 -0500

    Added Create Functionality in User Controller. Fixed Pom file issues

--- src/main/java/com/thebookcooper/controller/UserController.java
+++ src/main/java/com/thebookcooper/controller/UserController.java
@@ -1,5 +1,7 @@
 package com.thebookcooper.controller;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.thebookcooper.dao.DatabaseConnectionManager;
 import com.thebookcooper.model.User;
 import com.thebookcooper.dao.UserDAO;
@@ -9,21 +11,33 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 import java.sql.Connection;
 import java.sql.SQLException;
+import java.sql.Timestamp;
+import java.util.Map;
 
 @RestController
 public class UserController {
 
-    private final DatabaseConnectionManager dcm = new DatabaseConnectionManager("localhost", 5555, "thebookcooper", "BCdev", "password");
+    @PostMapping("/users/create")
+    public User createUser(@RequestBody String json) throws JsonProcessingException {
+        ObjectMapper objectMapper = new ObjectMapper();
+        Map inputMap = objectMapper.readValue(json, Map.class);
 
-    @PostMapping("/users")
-    public User createUser(@RequestBody User newUser) {
-        try (Connection connection = dcm.getConnection()) {
+        DatabaseConnectionManager dcm = new DatabaseConnectionManager("db", 5432, "thebookcooper", "BCdev", "password");
+
+        User newUser = new User();
+    
+        try{ 
+            Connection connection = dcm.getConnection();
             UserDAO userDAO = new UserDAO(connection);
-            // Assuming newUser already contains all the necessary fields set from the request body
+            newUser.setUserName((String) inputMap.get("userName"));
+            newUser.setPassword((String) inputMap.get("password"));
+            newUser.setEmail((String) inputMap.get("email"));
+            newUser.setBBucksBalance((double) inputMap.get("bBucksBalance"));
+            newUser.setCreationDate(new Timestamp(System.currentTimeMillis()));
+            newUser.setLastLogin(new Timestamp(System.currentTimeMillis()));
             return userDAO.create(newUser);
         } catch (SQLException e) {
             e.printStackTrace();
-            // In a real application, you would handle this exception more gracefully
             throw new RuntimeException("Failed to create the user", e);
         }
     }
