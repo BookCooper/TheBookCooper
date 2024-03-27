22acd821ce95c72bbedae6bb78e48e6a05aedd4e
Aidan Cusa <aidancusa@gmail.com>
2024-03-27 13:41:06 -0400

    Added router functionality

--- theBookCooperUI/src/App.js
+++ theBookCooperUI/src/App.js
@@ -2,7 +2,32 @@ import React from 'react';
 import './App.css';
 import LandingPage from './components/LandingPage';
 
-function App() {
+/*
+function Home() {
+    return (
+        <div>
+            <h1>My Website</h1>
+        </div>
+    );
+}*/
+
+export function About() {
+    return (
+        <div>
+            <h1>About Us</h1>
+        </div>
+    );
+}
+
+export function Contact() {
+    return (
+        <div>
+            <h1>Contact Us</h1>
+        </div>
+    );
+}
+
+export function App() {
     return (
         <div className="App">
             <LandingPage />
@@ -10,4 +35,4 @@ function App() {
     );
 }
 
-export default App;
+//export default App;
--- theBookCooperUI/src/index.js
+++ theBookCooperUI/src/index.js
@@ -1,14 +1,36 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
+import {App, About, Contact} from './App';
 import reportWebVitals from './reportWebVitals';
 
+/*
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
+);*/
+
+import {
+    BrowserRouter,
+    Routes,
+    Route
+} from "react-router-dom";
+
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+    <BrowserRouter>
+        <Routes>
+            <Route path="/" element={<App />} />
+            <Route path="/about" element={<About />} />
+            <Route
+                path="/contact"
+                element={<Contact />}
+            />
+        </Routes>
+    </BrowserRouter>,
 );
 
 // If you want to start measuring performance in your app, pass a function
