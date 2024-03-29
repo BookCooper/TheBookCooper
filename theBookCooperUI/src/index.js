0ef95ce07ef882d40bafaf6eec5af93042870441
Aidan Cusa <aidan.cusa@cooper.edu>
2024-03-29 19:44:59 -0400

    Fixed css boilerplate code

--- theBookCooperUI/src/index.js
+++ theBookCooperUI/src/index.js
@@ -8,14 +8,6 @@ import LoginPage from "./pages/LoginPage";
 import SignupPage from "./pages/SignupPage";
 import reportWebVitals from './reportWebVitals';
 
-/*
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);*/
-
 import {
     BrowserRouter,
     Routes,
