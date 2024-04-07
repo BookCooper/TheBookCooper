bd83986e9e9abb3d6d883ef6299d97f165fa9cfb
Aidan Cusa <aidancusa@gmail.com>
2024-04-07 12:34:42 -0400

    Added database test with axios

--- theBookCooperUI/src/components/Dashboard.js
+++ theBookCooperUI/src/components/Dashboard.js
@@ -1,9 +1,29 @@
-import React from 'react';
 import '../styles/LandingPage.css';
+import React, { useState, useEffect } from 'react';
+
+function ShowListings() {
+    const [data, setData] = useState(null);
+    useEffect(() => {
+        fetch(
+            `http://localhost:8080/api/getUsers`
+        )
+            .then((response) => response.json())
+            .then(setData);
+    }, []);
+    if (data)
+        return (
+            <>
+                <pre>{JSON.stringify(data, null, 2)}</pre>
+            </>
+        );
+    return <h1>Data</h1>;
+}
+
 
 const Dashboard = () => {
     return (
         <div>
+            {ShowListings()}
             <h2> Dashboard </h2>
         </div>
     );
