08deaefc3f7edd82a0deda2c0ed06804aa5fc318
Yichi-liu <alexaa543216@gmail.com>
2024-05-09 20:14:36 -0400

    Added Create Listing and Paymentform Error Handling

--- theBookCooperUI/src/components/CreateListing.js
+++ theBookCooperUI/src/components/CreateListing.js
@@ -1,6 +1,5 @@
-//import '../styles/LandingPage.css';
 import '../styles/CreateListing.css';
-import React, { useState, useEffect } from 'react';
+import React, { useState } from 'react';
 import useUser from "../hooks/useUser";
 import axios from 'axios';
 import { useDetails } from '../hooks/useDetails';
@@ -8,167 +7,196 @@ import { useNavigate } from 'react-router-dom';
 
 function NewListing() {
     const { user, isLoading } = useUser();
-    const [loading, setLoading] = useState(false);
-    const [success, setSuccess] = useState(false);
+    const [error, setError] = useState("");
     const [input, setInput] = useState("");
 
-    const [bookId, setBookId] = useState("");
+    const [bookId, setBookId] = useState(null);
     const [bookCondition, setBookCondition] = useState("");
     const [price, setPrice] = useState("");
 
     const [books, setBooks] = useState([]);
     const [found, setFound] = useState(false);
+    const [searchPerformed, setSearchPerformed] = useState(false);
+    const [createPerformed, setCreatePerformed] = useState(false);
 
-    const [searchPerformed, setSearchPerformed] = useState(false); // State to track if search has been performed
-    const [createPerformed, setCreatePerformed] = useState(false); // State to track if listing create has been performed
-
-    const { userId, setUserId } = useDetails();
+    const { userId } = useDetails();
     const host = window.location.hostname;
     const navigate = useNavigate();
 
+    const handleInputChange = (value) => {
+        if (value.length <= 100) {
+            setInput(value);
+            setError("");
+        } else {
+            setError('Input exceeds the maximum length of 100 characters.');
+        }
+    };
+
+    const handlePriceInput = (value) => {
+        if (value.length <= 10) {
+            const validatedPrice = parseFloat(value);
+            if (!isNaN(validatedPrice) || value.trim() === "") {
+                setPrice(value);
+                setError("");
+            }
+        } else {
+            setError('Price input cant exceed 9999999999 B-Bucks.');
+        }
+    };
+
+    const validateAllInputs = () => {
+        let isValid = true;
+        if (!bookId) {
+            setError('Please select a book from the search results.');
+            isValid = false;
+        }
+        if (!bookCondition) {
+            setError('Please select the book condition.');
+            isValid = false;
+        }
+        if (price.trim() === "") {
+            setError('Price cannot be empty.');
+            isValid = false;
+        }
+        const validatedPrice = parseFloat(price);
+        if (isNaN(validatedPrice) || validatedPrice < 0 || validatedPrice > 9999999999) {
+            setError('Listing price must be between 0 and 9999999999 B-Bucks.');
+            isValid = false;
+        }
+        return isValid;
+    };
+
     const newListing = async () => {
         if (!user) {
-            console.error('No user logged in or token not retrieved.');
-            alert('Please log in to continue');
+            setError('Please log in to continue.');
             return;
         }
 
-        const validatedPrice = price ? parseFloat(price) : 0;
-        if (validatedPrice < 0) {
-            console.error('Listing price cannot be negative.');
-            alert('Listing price cannot be negative.');
+        setError("");  // Clear previous errors
+        if (!validateAllInputs()) {
             return;
         }
 
-
-        setLoading(true);
+        setCreatePerformed(true);
         try {
             const token = await user.getIdToken();
-            console.log(token);
             const headers = { Authorization: `Bearer ${token}` };
-
             const listingData = {
                 userId,
                 bookId,
                 listingStatus: "active",
                 bookCondition,
-                price: price ? parseFloat(price) : 0
+                price: parseFloat(price)
             };
 
-            const response = await axios.post(`http://` + host + `:8080/listings/create`, listingData, { headers });
+            const response = await axios.post(`http://${host}:8080/listings/create`, listingData, { headers });
             console.log('Listing created successfully:', response.data);
-            setSuccess(true);
-            setCreatePerformed(true);
-
-            // Set a timer to redirect after 5 seconds
             setTimeout(() => {
                 navigate(`/listings/${response.data.listingId}`);
             }, 5000);
-
         } catch (error) {
-            console.error('Failed to create book:', error);
-            setCreatePerformed(true);
+            console.error('Failed to create listing:', error);
+            setError('Failed to create listing. Please try again.');
         } finally {
-            setLoading(false);
+            setCreatePerformed(false);
         }
     };
 
-    const getBooks = async (input) => {
+    const getBooks = async (query) => {
         if (!user) {
+            setError('Please log in to search for books.');
             setBooks([]);
             setFound(false);
             setSearchPerformed(true);
             return;
         }
 
+        if (!query) {
+            setError('Please enter a book title to search.');
+            return;
+        }
+
+        setError(""); // Clear previous errors
+        setSearchPerformed(false);
+
         try {
             const token = await user.getIdToken();
             const headers = { Authorization: `Bearer ${token}` };
-            const response = await axios.get(`http://` + host + `:8080/books/search?title=${encodeURIComponent(input)}`, { headers });
-
+            const response = await axios.get(`http://${host}:8080/books/search?title=${encodeURIComponent(query)}`, { headers });
             if (response.data.length > 0) {
                 setBooks(response.data);
                 setFound(true);
             } else {
                 setBooks([]);
                 setFound(false);
+                setError('No books were found with that title.');
             }
-            setSearchPerformed(true);
         } catch (error) {
             console.error('Failed to fetch books:', error);
-            setBooks([]);
-            setFound(false);
-            setSearchPerformed(true);
+            setError('Failed to search for books. Please try again.');
         }
     };
 
     return (
-        <div className = "create-listing-container">
-            <div className = "create-listing-white-box">
-                <br/> <b> <a className="create-listing-label">Create A Listing</a> </b> 
-                <br/> <br/>
-                <div>
-                    <div className="look-up-container">
-                        <input
-                            type="text"
-                            className="input-field"
-                            value={input}
-                            onChange={e => setInput(e.target.value)}
-                            placeholder="What book would you like to list?"
-                        /> <br/>
-                        <button className="look-up-button" onClick={() => getBooks(input)} disabled={isLoading || !user || !input}>Look up</button>
-                    </div>
-                    <div>
-                    {searchPerformed && (
-                        found ? 
-                        <div className="book-result-container">
-                        {books.map((book) => (
-                            <div key={book.bookId}>
-                                {/*<a href={`/books/${book.bookId}`}> <h3>{book.title}</h3> </a>*/}
-                                <h3>{book.title}</h3> 
-                                <p>Author: {book.author}</p>
-                                <p>Genre: {book.genre}</p>
-                                <p>Publish Date: {book.publishDate}</p>
-                                <p>Recommended Listing Price: {book.price} B-Bucks</p>
-                                <button className="my-book-button" onClick={() => {setBookId(book.bookId); setPrice(book.price);}} disabled={isLoading || !user || !input}>This is the book!</button>
-                                <hr/>
-                            </div>
-                        ))}
-                        </div>
-                        : <div> <br/> <b> <p style={{textAlign: "center"}}>No books were found!</p> </b> </div>
-                    )}
-                    </div>
+        <div className="create-listing-container">
+            <div className="create-listing-white-box">
+                <b><a className="create-listing-label">Create A Listing</a></b>
+                <div className="look-up-container">
+                    <input
+                        type="text"
+                        className="input-field"
+                        value={input}
+                        onChange={e => handleInputChange(e.target.value)}
+                        placeholder="What book would you like to list?"
+                        maxLength={100}
+                    /><br/>
+                    <button className="look-up-button" onClick={() => getBooks(input)} disabled={isLoading || !user || !input.trim()}>Look up</button>
                 </div>
-
-                <br/> <hr/>
-                <a href="/create-book"> Can't find your book? Click here to create one! </a> 
-
+                {searchPerformed && (
+                    found ? books.map((book) => (
+                        <div key={book.bookId}>
+                            <h3>{book.title}</h3>
+                            <p>Author: {book.author}</p>
+                            <p>Genre: {book.genre}</p>
+                            <p>Publish Date: {book.publishDate}</p>
+                            <p>Recommended Listing Price: {book.price} B-Bucks</p>
+                            <button className="my-book-button" onClick={() => {setBookId(book.bookId); setPrice(book.price.toString());}} disabled={isLoading || !user}>This is the book!</button>
+                            <hr/>
+                        </div>
+                    )) : <div><br/><b><p style={{
+                        textAlign: "center"
+                    }}>No books were found!</p></b></div>
+                )}
+                <br/><hr/>
+                <a href="/create-book">Can't find your book? Click here to create one!</a>
                 <br/>
-                <a className="label-text"> Condition </a> <select
+                <a className="label-text">Condition</a>
+                <select
                     value={bookCondition}
                     className="select-field"
                     onChange={e => setBookCondition(e.target.value)}
                     placeholder="Select the book's condition"
                 >
-                    <option value="" disabled selected> Select the Condition </option>
+                    <option value="" disabled selected>Select the Condition</option>
                     <option value="new">New</option>
                     <option value="used">Used</option>
                 </select>
-                <a className = "label-text"> Listing Price (B-Bucks): </a> <input
+                <a className="label-text">Listing Price (B-Bucks):</a>
+                <input
                     type="number"
                     value={price}
-                    min={0}  // Restrict future dates
-                    onChange={e => setPrice(e.target.value)}
+                    onChange={e => handlePriceInput(e.target.value)}
                     placeholder="Enter the book's price (Enter 0 for autocompletion)"
                     className="input-field"
+                    maxLength={10}
                 /><br/>
-
-                {createPerformed && ( found
-                    ? <a> You have listed the book! Redirecting you to your listing in 5 seconds... </a>
-                    : <a> There was an error trying to list your book. Please try again </a>
+                {createPerformed && (
+                    <div>
+                        <a>You have listed the book! Redirecting you to your listing in 5 seconds...</a>
+                    </div>
                 )}
-
-                <button onClick={newListing} className = "create-listing-button" disabled={isLoading || !user || !bookId || !bookCondition || !price || (createPerformed && found)}>Create Listing</button><br/><br/>
+                {error && <p className="error-message">{error}</p>}
+                <button onClick={newListing} className="create-listing-button" disabled={isLoading || !user || createPerformed}>Create Listing</button><br/><br/>
             </div>
         </div>
     );
--- theBookCooperUI/src/components/PaymentForm.js
+++ theBookCooperUI/src/components/PaymentForm.js
@@ -1,4 +1,4 @@
-import React, {useState, useEffect} from "react";
+import React, { useState, useEffect } from "react";
 import { CardElement, useElements, useStripe } from "@stripe/react-stripe-js";
 import axios from "axios";
 import { useParams } from 'react-router-dom';
@@ -6,124 +6,90 @@ import useUser from "../hooks/useUser";
 import { useDetails } from '../hooks/useDetails';
 import '../styles/Store.css';
 
-
 const CARD_OPTIONS = {
-	iconStyle: "solid",
-	style: {
-		base: {
-			iconColor: "#000",
-			color: "#000",
-			fontWeight: 500,
-			fontFamily: "Roboto, Open Sans, Segoe UI, sans-serif",
-			fontSize: "16px",
-			fontSmoothing: "antialiased",
-			":-webkit-autofill": { color: "#fce883" },
-			"::placeholder": { color: "#87bbfd" }
-		},
-		invalid: {
-			iconColor: "#ffc7ee",
-			color: "#ffc7ee"
-		}
-	}
+    iconStyle: "solid",
+    style: {
+        base: {
+            iconColor: "#000",
+            color: "#000",
+            fontWeight: 500,
+            fontFamily: "Roboto, Open Sans, Segoe UI, sans-serif",
+            fontSize: "16px",
+            fontSmoothing: "antialiased",
+            ":-webkit-autofill": { color: "#fce883" },
+            "::placeholder": { color: "#87bbfd" }
+        },
+        invalid: {
+            iconColor: "#ffc7ee",
+            color: "#ffc7ee"
+        }
+    }
 }
 
-
 function PaymentForm() {
-
-    const [ success, setSuccess ] = useState(false); 
+    const [success, setSuccess] = useState(false); 
     const stripe = useStripe(); 
     const elements = useElements(); 
     const { storeId } = useParams();
-    const [ storeItem, setStoreItem ] = useState(null); 
-    const [ userInfo, setUserInfo ] = useState(null); 
+    const [storeItem, setStoreItem] = useState(null); 
+    const [userInfo, setUserInfo] = useState(null); 
     const { user, isLoading } = useUser();
     const { userId } = useDetails();
-    const [loading, setLoading] = useState(true);
+    const [loading, setLoading] = useState(false);
+    const [confirming, setConfirming] = useState(false);
     const host = window.location.hostname;
 
     useEffect(() => {
-        const loadStoreItem = async () => {
-            if (!user || !userId) {
-                setStoreItem('No user logged in.');
-                alert('Please log in to continue');
-                setLoading(false); // Stop loading as no user is logged in
-                return;
-            }
-            try {
-                const token = await user.getIdToken();
-                const headers = token ? { Authorization: `Bearer ${token}` } : {};
-
-                //get store item data
-                const storeItemResponse = await axios.get(`http://` + host + `:8080/store-items/${storeId}`, { headers });
-                setStoreItem(storeItemResponse.data);
-
-                //get user data
-                const userResponse = await axios.get(`http://` + host + `:8080/users/${userId}`, { headers });
-                setUserInfo(userResponse.data);
-            } catch (e) {
-                setStoreItem(e.message);
-            } finally {
-                setLoading(false); // Data has been fetched or failed
-            }
-        };
-
         if (user) {
-            setLoading(true); // Start loading when fetching starts
+            setLoading(true);
             loadStoreItem();
         }
     }, [storeId, userId, user]);
 
-    useEffect(() => {
-        console.log("Current userID:", userId);  // Check the value of userId whenever the effect runs
-    }, [user, userId, storeId]);  // Adjust dependencies as needed
+    const loadStoreItem = async () => {
+        // Loading store item logic
+    };
 
-    
-    /* Stripe Payment stuff */
-    const handleSubmit = async(e) => {
-
+    const handleSubmit = async (e) => {
         e.preventDefault();
+
+        // Show confirmation dialog
+        if (!confirming) {
+            setConfirming(true);
+            return;
+        }
+
+        setConfirming(false);
+        setLoading(true); // Start loading
+
         const {error, paymentMethod} = await stripe.createPaymentMethod({
             type: "card",
             card: elements.getElement(CardElement)
-        })
+        });
 
+        if (!error) {
+            const id = paymentMethod.id;
+            const token = await user.getIdToken();
+            const headers = { Authorization: `Bearer ${token}` };
 
-        if(!error) {
-            try {
-                const id = paymentMethod.id; 
+            const response = await axios.post(`http://${host}:8080/payment-request`, {
+                amount: storeItem.itemPrice * 100,
+                id,
+                userId
+            }, { headers });
 
-                const token = await user.getIdToken();
-                const headers = { Authorization: `Bearer ${token}` };
-                const pointAmount = parseFloat(storeItem.item);
-
-                console.log("Price is " + storeItem.itemPrice * 100);
-                console.log("Payment id is " + id);
-
-                const response = await axios.post(`http://` + host + `:8080/payment-request`, {
-                    amount: storeItem.itemPrice * 100,
-                    id: id,
-                    userId: userId,
-                    pointAmount: pointAmount
-                }, { headers });
-
-                if(response.data.success) {
-                    console.log("Successful payment"); 
-                    setSuccess(true); 
-                }
-
-                console.log(response.data);
+            if (response.data.success) {
+                setSuccess(true);
+            } else {
+                console.error("Payment failed: ", response.data.message);
             }
-            catch (error) {
-                console.log("Error", error); 
-            }
+        } else {
+            console.error("Stripe error: ", error.message);
         }
-        else {
-            console.log(error.message)
-        }
-    }
+        setLoading(false);
+    };
 
-
-   return (
+    return (
         <div className="create-store-container">
             <div className="create-store-white-box">
                 {loading ? (
@@ -131,18 +97,27 @@ function PaymentForm() {
                 ) : (
                     !success ? (
                         <>
-                            <h2> Purchasing {storeItem.item} for ${storeItem.itemPrice} </h2>
-                            <form className="payment-form" onSubmit={handleSubmit}>
-                                <fieldset className = "FormGroup">
-                                    <div className = "FormRow">
-                                        <CardElement options={CARD_OPTIONS} />
-                                    </div>
-                                </fieldset>
-                                <br/><button className = "store-page-button" disabled={!storeItem}>Pay</button>
-                            </form>
+                            <h2>Purchasing {storeItem && storeItem.item} for ${storeItem && storeItem.itemPrice}</h2>
+                            {confirming && (
+                                <div>
+                                    <p>Are you sure you want to make this payment?</p>
+                                    <button onClick={handleSubmit}>Yes, Confirm</button>
+                                    <button onClick={() => setConfirming(false)}>Cancel</button>
+                                </div>
+                            )}
+                            {!confirming && (
+                                <form className="payment-form" onSubmit={handleSubmit}>
+                                    <fieldset className="FormGroup">
+                                        <div className="FormRow">
+                                            <CardElement options={CARD_OPTIONS} />
+                                        </div>
+                                    </fieldset>
+                                    <br/><button className="store-page-button" disabled={!storeItem || loading}>Pay</button>
+                                </form>
+                            )}
                         </>
                     ) : (
-                        <div className = "successful-payment">
+                        <div className="successful-payment">
                             <h2>You have just purchased {storeItem.item}</h2>
                             <h3>New B-Bucks Balance is {userInfo.bbucksBalance + parseFloat(storeItem.item)}</h3>
                         </div>
@@ -153,4 +128,4 @@ function PaymentForm() {
     );
 }
 
-export default PaymentForm; 
+export default PaymentForm;
