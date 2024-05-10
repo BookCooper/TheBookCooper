fc4e5b2fc918b9b4f0bd482ae9ecbec524fc6ccc
Yichi-liu <alexaa543216@gmail.com>
2024-05-09 23:12:26 -0400

    Merge

--- theBookCooperUI/src/components/PaymentForm.js
+++ theBookCooperUI/src/components/PaymentForm.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import React, {useState, useEffect} from "react";
 import { CardElement, useElements, useStripe } from "@stripe/react-stripe-js";
 import axios from "axios";
 import { useParams } from 'react-router-dom';
@@ -6,37 +6,39 @@ import useUser from "../hooks/useUser";
 import { useDetails } from '../hooks/useDetails';
 import '../styles/Store.css';
 
+
 const CARD_OPTIONS = {
-    iconStyle: "solid",
-    style: {
-        base: {
-            iconColor: "#000",
-            color: "#000",
-            fontWeight: 500,
-            fontFamily: "Roboto, Open Sans, Segoe UI, sans-serif",
-            fontSize: "16px",
-            fontSmoothing: "antialiased",
-            ":-webkit-autofill": { color: "#fce883" },
-            "::placeholder": { color: "#87bbfd" }
-        },
-        invalid: {
-            iconColor: "#ffc7ee",
-            color: "#ffc7ee"
-        }
-    }
+	iconStyle: "solid",
+	style: {
+		base: {
+			iconColor: "#000",
+			color: "#000",
+			fontWeight: 500,
+			fontFamily: "Roboto, Open Sans, Segoe UI, sans-serif",
+			fontSize: "16px",
+			fontSmoothing: "antialiased",
+			":-webkit-autofill": { color: "#fce883" },
+			"::placeholder": { color: "#87bbfd" }
+		},
+		invalid: {
+			iconColor: "#ffc7ee",
+			color: "#ffc7ee"
+		}
+	}
 }
 
+
 function PaymentForm() {
-    const [success, setSuccess] = useState(false); 
+
+    const [ success, setSuccess ] = useState(false); 
     const stripe = useStripe(); 
     const elements = useElements(); 
     const { storeId } = useParams();
-    const [storeItem, setStoreItem] = useState(null); 
-    const [userInfo, setUserInfo] = useState(null); 
+    const [ storeItem, setStoreItem ] = useState(null); 
+    const [ userInfo, setUserInfo ] = useState(null); 
     const { user, isLoading } = useUser();
     const { userId } = useDetails();
-    const [loading, setLoading] = useState(false);
-    const [confirming, setConfirming] = useState(false);
+    const [loading, setLoading] = useState(true);
     const host = window.location.hostname;
 
     useEffect(() => {
@@ -75,77 +77,80 @@ function PaymentForm() {
         console.log("Current userID:", userId);  // Check the value of userId whenever the effect runs
     }, [user, userId, storeId]);  // Adjust dependencies as needed
 
-
-    const handleSubmit = async (e) => {
+    
+    /* Stripe Payment stuff */
+    const handleSubmit = async(e) => {
 
         e.preventDefault();
-        setLoading(true); // Start loading
         const {error, paymentMethod} = await stripe.createPaymentMethod({
             type: "card",
             card: elements.getElement(CardElement)
         })
-        // Show confirmation dialog
-        if (!confirming) {
-            setConfirming(true);
-            return;
-        }
-
-        setConfirming(false);
-
-        if (!error) {
-            const id = paymentMethod.id;
-            const token = await user.getIdToken();
-            const headers = { Authorization: `Bearer ${token}` };
-            const pointAmount = parseFloat(storeItem.item);
 
 
-            const response = await axios.post(`http://` + host + `:8080/payment-request`, {
-                amount: storeItem.itemPrice * 100,
-                id: id,
-                userId: userId,
-                pointAmount: pointAmount
-            }, { headers });
+        if(!error) {
+            try {
+                const id = paymentMethod.id; 
 
-            if (response.data.success) {
-                console.log("Successful payment"); 
-                setSuccess(true);
-            } else {
-                console.error("Payment failed: ", response.data.message);
+                const token = await user.getIdToken();
+                const headers = { Authorization: `Bearer ${token}` };
+                const pointAmount = parseFloat(storeItem.item);
+
+                console.log("Price is " + storeItem.itemPrice * 100);
+                console.log("Payment id is " + id);
+
+                const response = await axios.post(`http://` + host + `:8080/payment-request`, {
+                    amount: storeItem.itemPrice * 100,
+                    id: id,
+                    userId: userId,
+                    pointAmount: pointAmount
+                }, { headers });
+
+                if(response.data.success) {
+                    console.log("Successful payment"); 
+                    setSuccess(true); 
+                }
+
+                console.log(response.data);
             }
-        } else {
-            console.error("Stripe error: ", error.message);
+            catch (error) {
+                console.log("Error", error); 
+            }
         }
-        setLoading(false);
-    };
+        else {
+            console.log(error.message)
+        }
+    }
 
-    return (
+
+   return (
         <div className="create-store-container">
             <div className="create-store-white-box">
                 {loading ? (
                     <p>Loading...</p>
                 ) : (
                     !success ? (
-                        <form className="payment-form" onSubmit={handleSubmit}>
-                        <h2>Purchasing {storeItem && storeItem.item} for ${storeItem && storeItem.itemPrice}</h2>
-                        <fieldset className="FormGroup">
-                            <div className="FormRow">
-                                <CardElement options={CARD_OPTIONS} />
-                            </div>
-                        </fieldset>
-                        <br/>
-                        <button className="store-page-button" disabled={!storeItem || loading}>Pay</button>
-                    </form>
-                ) : (
-                    <div className="successful-payment">
-                        <h2>You have just purchased {storeItem.item}</h2>
-                        <h3>New B-Bucks Balance is {userInfo.bbucksBalance + parseFloat(storeItem.item)}</h3>
-                    </div>
-                )
-            )}
+                        <>
+                            <h2> Purchasing {storeItem.item} for ${storeItem.itemPrice} </h2>
+                            <form className="payment-form" onSubmit={handleSubmit}>
+                                <fieldset className = "FormGroup">
+                                    <div className = "FormRow">
+                                        <CardElement options={CARD_OPTIONS} />
+                                    </div>
+                                </fieldset>
+                                <br/><button className = "store-page-button" disabled={!storeItem}>Pay</button>
+                            </form>
+                        </>
+                    ) : (
+                        <div className = "successful-payment">
+                            <h2>You have just purchased {storeItem.item}</h2>
+                            <h3>New B-Bucks Balance is {userInfo.bbucksBalance + parseFloat(storeItem.item)}</h3>
+                        </div>
+                    )
+                )}
+            </div>
         </div>
-    </div>
-);
+    );
 }
 
-
-export default PaymentForm;
+export default PaymentForm; 
